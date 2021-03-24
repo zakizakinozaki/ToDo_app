d34847bf1f5b92393425fbfcab44932ba41c4534
zakizakinozaki <78135984+zakizakinozaki@users.noreply.github.com>
2021-03-24 15:49:04 +0900

    devise制限の記述変更

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  before_action :authenticate_user!, expect: :index
+  before_action :authenticate_user!, except: [:index]
   before_action :set_list, only: [:edit, :update, :destroy]
 
   def index
