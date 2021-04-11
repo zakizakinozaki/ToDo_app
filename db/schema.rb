52252f9f89a4af5922608d2b935d3305467629ec
zakizakinozaki <78135984+zakizakinozaki@users.noreply.github.com>
2021-04-12 05:32:31 +0900

    rspec導入

--- db/schema.rb
+++ db/schema.rb
@@ -66,15 +66,6 @@ ActiveRecord::Schema.define(version: 2021_04_06_085347) do
     t.index ["user_id"], name: "index_outputs_on_user_id"
   end
 
-  create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "title", null: false
-    t.text "quest", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_questions_on_user_id"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
@@ -92,5 +83,4 @@ ActiveRecord::Schema.define(version: 2021_04_06_085347) do
   add_foreign_key "cards", "lists"
   add_foreign_key "lists", "users"
   add_foreign_key "outputs", "users"
-  add_foreign_key "questions", "users"
 end
